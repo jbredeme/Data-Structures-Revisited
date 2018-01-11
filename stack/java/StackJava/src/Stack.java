510d273a306038c4ceb341c9e6072271ee0f58cc
jbredeme <jpb64@nau.edu>
2018-01-10 21:44:59 -0700

    Simple update

--- stack/java/StackJava/src/Stack.java
+++ stack/java/StackJava/src/Stack.java
@@ -3,6 +3,20 @@
  * @author Jarid Bredemeier
  *
  */
-public class Stack {
-
+public abstract class Stack {
+	private int size;
+	
+	public Stack() {
+		size = 0;
+		
+	}
+	
+	public int size() {
+		return size;
+		
+	}
+	
+	public abstract Node pop();
+	public abstract boolean push();
+ 	
 }
--- stack/java/StackJava/src/StackApp.java
+++ stack/java/StackJava/src/StackApp.java
@@ -3,10 +3,53 @@
  * @author Jarid Bredemeier
  *
  */
+
+import java.util.*;
+
 public class StackApp {
 
 	public static void main(String[] args) {
+		int max = 1024;
+		Random ran = new Random();
+		
+		// Collections employment
+		LinkedList<Node> stack = new LinkedList<Node>();
+		
+		// Push elements onto the stack
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+		stack.push(new Node(ran.nextInt(max)));
+
+		System.out.println(displayStack(stack));
+		
+		// Remove elements on the stack
+		for(int i = 0; i < 4; i++) {
+			System.out.println("Removing: " + stack.pop());
+			
+		}
+		
+		System.out.println(displayStack(stack));
 		
 	}
+	
+	public static String displayStack(LinkedList<Node> stack) {
+		String out = new String();
+		
+		// Display elements currently on the stack
+		for(Node x : stack) {
+			out += x.getPayload() + " ";
+			
+		}
+		
+		return out;
+	}
+	
+	
+	
 	
 }
